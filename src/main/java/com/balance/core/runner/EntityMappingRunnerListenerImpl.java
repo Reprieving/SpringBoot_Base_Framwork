e670957017502f308c039be8d598f63bded72aa7
zzz <shoppine4lee@gmail.com>
2018-08-10 17:36:14 +0800

    ~

--- src/main/java/com/balance/core/runner/EntityMappingRunnerListenerImpl.java
+++ src/main/java/com/balance/core/runner/EntityMappingRunnerListenerImpl.java
@@ -19,7 +19,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
 
-@Component
+//@Component
 public class EntityMappingRunnerListenerImpl implements ApplicationListener {
 
     @Resource
