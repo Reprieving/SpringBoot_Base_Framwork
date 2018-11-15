0d0c109f9a152aed59b0ad3a944e61672d6a4eba
dengshengbin <dengshengbin>
2018-11-15 22:27:47 +0800

    ~

--- src/main/java/com/balance/mapper/user/UserMapper.java
+++ src/main/java/com/balance/mapper/user/UserMapper.java
@@ -1,9 +1,8 @@
 package com.balance.mapper.user;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserMapper {
-    Integer selectUserByUserName(@Param("userName") String userName);
+
 }
